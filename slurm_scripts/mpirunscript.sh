93f8f65401ddd3f710f11f37c171e91222e0c613
Michael Rosenberg <micro@login-1.zaratan.umd.edu>
2023-10-17 18:30:16 -0400

    MPI scripts producing reasonable log names

--- slurm_scripts/mpirunscript.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-module load openmpi
-echo "about to run"
-mpirun -n 9 /home/micro/mpitest/node --num-workers 8 --num-subcircuits 8 --num-sha2-iters 1 --num-portals 1
--- slurm_scripts/myrun.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sbatch \
-	--wait \
-	--ntasks=9 \
-       	--out=srun_out.txt \
-	--error=srun_err.txt \
-	mpirunscript.sh
