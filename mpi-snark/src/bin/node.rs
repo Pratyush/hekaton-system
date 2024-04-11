fbcd20cd0fbc0a362a8a78622ac7edf3eb256ecc
Michael Rosenberg <michael@mrosenberg.pub>
2024-04-11 00:07:57 -0400

    Ripped MPI out of failing verif case

--- mpi-snark/src/bin/node.rs
+++ mpi-snark/src/bin/node.rs
@@ -1,24 +1,14 @@
-#![allow(warnings)]
 use ark_std::{cfg_chunks, cfg_chunks_mut, cfg_into_iter, cfg_iter};
 use distributed_prover::tree_hash_circuit::MerkleTreeCircuitParams;
 
 use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
 use clap::{Parser, Subcommand};
 use mimalloc::MiMalloc;
-use mpi::{
-    datatype::{Partition, PartitionMut},
-    topology::Process,
-    Count,
-};
-use mpi::{request, traits::*};
 use mpi_snark::{
-    construct_partitioned_buffer_for_scatter, construct_partitioned_mut_buffer_for_gather,
     coordinator::{generate_g16_pks, CoordinatorState},
     data_structures::{ProvingKeys, Stage0Request, Stage0Response, Stage1Request, Stage1Response},
-    deserialize_flattened_bytes, deserialize_from_packed_bytes, serialize_to_packed_vec,
-    serialize_to_vec,
+    deserialize_flattened_bytes, serialize_to_vec,
     worker::WorkerState,
-    Packed,
 };
 
 use std::{
@@ -171,247 +161,65 @@ fn setup(
 }
 
 fn work(num_workers: usize, proving_keys: ProvingKeys) {
-    let (universe, _) = mpi::initialize_with_threading(mpi::Threading::Funneled).unwrap();
-    let world = universe.world();
-    let root_rank = 0;
-    let root_process = world.process_at_rank(root_rank);
-    let rank = world.rank();
-    let size = world.size();
-
     let circ_params = proving_keys.circ_params.clone();
     let num_subcircuits = 2 * circ_params.num_leaves;
+    let current_num_threads = current_num_threads() - 1;
 
     let num_subcircuits_per_worker = num_subcircuits / num_workers;
     assert_eq!(num_subcircuits_per_worker * num_workers, num_subcircuits);
 
     let mut log = Vec::new();
-    let very_start = start_timer_buf!(log, || format!("Node {rank}: Beginning work"));
+    let very_start = start_timer_buf!(log, || format!("Node: Beginning work"));
 
-    if rank == root_rank {
-        // Initial broadcast
+    // Initial broadcast
 
-        let start = start_timer_buf!(log, || format!("Coord: construct coordinator state"));
-        let mut coordinator_state = CoordinatorState::new(&proving_keys);
-        end_timer_buf!(log, start);
+    let start = start_timer_buf!(log, || format!("Coord: construct coordinator state"));
+    let mut coordinator_state = CoordinatorState::new(&proving_keys);
+    end_timer_buf!(log, start);
 
-        /***************************************************************************/
-        /***************************************************************************/
-        // Stage 0
-        let start = start_timer_buf!(log, || format!("Coord: Generating stage0 requests"));
-        let requests = coordinator_state.stage_0();
-        let requests_chunked = requests
-            .chunks(num_subcircuits_per_worker)
-            .map(|chunk| chunk.to_vec())
+    /***************************************************************************/
+    /***************************************************************************/
+    // Stage 0
+    let start = start_timer_buf!(log, || format!("Coord: Generating stage0 requests"));
+    let requests = coordinator_state.stage_0();
+    end_timer_buf!(log, start);
+
+    let mut worker_states =
+        std::iter::from_fn(|| Some(WorkerState::new(num_subcircuits, &proving_keys)))
+            .take(num_subcircuits_per_worker)
             .collect::<Vec<_>>();
-        end_timer_buf!(log, start);
 
-        // Stage 0 scatter
-        scatter_requests(&mut log, "stage0", &root_process, &requests_chunked);
-        println!("Finished coordinator scatter 0");
+    let start = start_timer_buf!(log, || format!("Worker: Processing stage0 requests"));
+    let responses = compute_responses(
+        current_num_threads,
+        &requests,
+        &mut worker_states,
+        |req, state| state.stage_0(rand::thread_rng(), &req),
+    );
+    end_timer_buf!(log, start);
+    println!("Finished worker scatter 0");
 
-        // Stage 0 gather
-        let default_response = vec![Stage0Response::default(); num_subcircuits_per_worker];
-        let responses_chunked: Vec<Vec<_>> =
-            gather_responses(&mut log, "stage0", size, &root_process, default_response);
-        let responses = cfg_into_iter!(responses_chunked)
-            .flatten()
-            .collect::<Vec<_>>();
-        println!("Finished coordinator gather 0");
-        /***************************************************************************/
-        /***************************************************************************/
-
-        /***************************************************************************/
-        /***************************************************************************/
-        // Stage 1
-        let start = start_timer_buf!(log, || format!("Coord: Processing stage0 responses"));
-        let requests = coordinator_state.stage_1(&responses);
-        let requests_chunked = requests
-            .chunks(num_subcircuits_per_worker)
-            .map(|chunk| chunk.to_vec())
-            .collect::<Vec<_>>();
-        end_timer_buf!(log, start);
-
-        // Stage 1 scatter
-        scatter_requests(&mut log, "stage1", &root_process, &requests_chunked);
-        println!("Finished coordinator scatter 1");
-
-        // Stage 1 gather
-        let default_response = vec![Stage1Response::default(); num_subcircuits_per_worker];
-        let responses_chunked: Vec<Vec<_>> =
-            gather_responses(&mut log, "stage1", size, &root_process, default_response);
-        let responses = cfg_into_iter!(responses_chunked)
-            .flatten()
-            .collect::<Vec<_>>();
-        println!("Finished coordinator gather 1");
-        /***************************************************************************/
-        /***************************************************************************/
-
-        let start = start_timer_buf!(log, || format!("Coord: Aggregating"));
-        let _proof = coordinator_state.aggregate(&responses);
-        end_timer_buf!(log, start);
-    } else {
-        let current_num_threads = current_num_threads() - 1;
-        println!(
-            "Rayon num threads in worker {rank}: {}",
-            current_num_threads
-        );
-        // Worker code
-        let start = start_timer_buf!(log, || format!("Worker {rank}: Initializing worker state"));
-        let mut worker_states =
-            std::iter::from_fn(|| Some(WorkerState::new(num_subcircuits, &proving_keys)))
-                .take(num_subcircuits_per_worker)
-                .collect::<Vec<_>>();
-        end_timer_buf!(log, start);
-
-        /***************************************************************************/
-        /***************************************************************************/
-        // Stage 0
-
-        // Receive Stage 0 request
-        let requests: Vec<Stage0Request> =
-            receive_requests(&mut log, rank, "stage0", &root_process);
-
-        // Compute Stage 0 response
-        let start = start_timer_buf!(log, || format!("Worker {rank}: Processing stage0 requests"));
-        let responses = compute_responses(
-            current_num_threads,
-            &requests,
-            &mut worker_states,
-            |req, state| state.stage_0(rand::thread_rng(), &req.to_ref()),
-        );
-        end_timer_buf!(log, start);
-        println!("Finished worker scatter 0 for rank {rank}");
-
-        // Send Stage 0 response
-        send_responses(&mut log, rank, "stage0", &root_process, &responses);
-        println!("Finished worker gather 0 for rank {rank}");
-
-        /***************************************************************************/
-        /***************************************************************************/
-
-        /***************************************************************************/
-        /***************************************************************************/
-        // Stage 1
-
-        // Receive Stage 1 request
-        let requests: Vec<Stage1Request> =
-            receive_requests(&mut log, rank, "stage1", &root_process);
-
-        // Compute Stage 1 response
-        let start = start_timer_buf!(log, || format!("Worker {rank}: Processing stage1 request"));
-        let responses = compute_responses(
-            current_num_threads,
-            &requests,
-            worker_states,
-            |req, state| state.stage_1(rand::thread_rng(), &req.to_ref()),
-        );
-        end_timer_buf!(log, start);
-        println!("Finished worker scatter 1 for rank {rank}");
-
-        send_responses(&mut log, rank, "stage1", &root_process, &responses);
-
-        println!("Finished worker gather 1 for rank {rank}");
-        /***************************************************************************/
-        /***************************************************************************/
-    }
-
-    end_timer_buf!(log, very_start);
-
-    println!("Rank {rank} log: {}", log.join(";"));
-}
-
-fn scatter_requests<'a, C: 'a + Communicator>(
-    log: &mut Vec<String>,
-    stage: &str,
-    root_process: &Process<'a, C>,
-    requests: &[impl CanonicalSerialize + Send],
-) {
-    let start = start_timer_buf!(log, || format!("Coord: Serializing {stage} requests"));
-    let mut request_bytes = vec![];
-    let request_bytes_buf = construct_partitioned_buffer_for_scatter!(requests, &mut request_bytes);
+    let start = start_timer_buf!(log, || format!("Coord: Processing stage0 responses"));
+    let requests = coordinator_state.stage_1(&responses);
     end_timer_buf!(log, start);
 
-    let counts = request_bytes_buf.counts().to_vec();
-    root_process.scatter_into_root(&counts, &mut 0i32);
-    let mut _recv_buf: Vec<u8> = vec![];
-
-    let start = start_timer_buf!(log, || format!("Coord: Scattering {stage} requests"));
-    root_process.scatter_varcount_into_root(&request_bytes_buf, &mut _recv_buf);
+    // Compute Stage 1 response
+    let start = start_timer_buf!(log, || format!("Worker: Processing stage1 request"));
+    let responses = compute_responses(
+        current_num_threads,
+        &requests,
+        worker_states,
+        |req, state| state.stage_1(rand::thread_rng(), &req),
+    );
     end_timer_buf!(log, start);
-}
+    println!("Finished worker scatter 1");
+    println!("Finished coordinator gather 1");
+    /***************************************************************************/
+    /***************************************************************************/
 
-fn receive_requests<'a, C: 'a + Communicator, T: CanonicalDeserialize>(
-    log: &mut Vec<String>,
-    rank: i32,
-    stage: &str,
-    root_process: &Process<'a, C>,
-) -> T {
-    let mut size = 0 as Count;
-    root_process.scatter_into(&mut size);
-
-    let start = start_timer_buf!(log, || format!(
-        "Worker {rank}: Receiving scattered {stage} request of size {size}"
-    ));
-    let mut request_ser = vec![Packed::zero(); size as usize];
-    root_process.scatter_varcount_into(&mut request_ser);
+    let start = start_timer_buf!(log, || format!("Coord: Aggregating"));
+    let _proof = coordinator_state.aggregate(&responses);
     end_timer_buf!(log, start);
-
-    let start = start_timer_buf!(log, || format!(
-        "Worker {rank}: Deserializing {stage} request"
-    ));
-    let ret = deserialize_from_packed_bytes(&request_ser[..]).unwrap();
-    end_timer_buf!(log, start);
-
-    ret
-}
-
-fn send_responses<'a, C: 'a + Communicator, T: CanonicalSerialize>(
-    log: &mut Vec<String>,
-    rank: i32,
-    stage: &str,
-    root_process: &Process<'a, C>,
-    responses: &[T],
-) {
-    // Send Stage 1 response
-    let start = start_timer_buf!(log, || format!(
-        "Worker {rank}: Serializing {stage} {}, responses",
-        responses.len(),
-    ));
-    let responses_bytes = serialize_to_vec(&responses);
-    end_timer_buf!(log, start);
-
-    let start = start_timer_buf!(log, || format!(
-        "Worker {rank}: Gathering {stage} response, each of size {}",
-        responses_bytes.len() / responses.len()
-    ));
-    root_process.gather_varcount_into(&responses_bytes);
-    end_timer_buf!(log, start);
-}
-
-fn gather_responses<'a, C, T>(
-    log: &mut Vec<String>,
-    stage: &str,
-    size: Count,
-    root_process: &Process<'a, C>,
-    default_response: T,
-) -> Vec<T>
-where
-    C: 'a + Communicator,
-    T: CanonicalSerialize + CanonicalDeserialize + Default,
-{
-    let mut response_bytes = vec![];
-    let mut response_bytes_buf =
-        construct_partitioned_mut_buffer_for_gather!(size, default_response, &mut response_bytes);
-    // Root does not send anything, it only receives.
-    let start = start_timer_buf!(log, || format!("Coord: Gathering {stage} responses"));
-    root_process.gather_varcount_into_root(&[0u8; 0], &mut response_bytes_buf);
-    end_timer_buf!(log, start);
-
-    let start = start_timer_buf!(log, || format!("Coord: Deserializing {stage} responses"));
-    let ret = deserialize_flattened_bytes!(response_bytes, default_response, T).unwrap();
-    end_timer_buf!(log, start);
-
-    ret
 }
 
 #[cfg(feature = "parallel")]
@@ -505,19 +313,9 @@ where
     U: Send + Sync,
     F: Send + Sync + Fn(&'a R, W) -> U,
 {
-    let (pool_size, chunk_size) = pool_and_chunk_size(current_num_threads(), requests.len());
-    let mut thread_results = Vec::new();
-    let chunks = worker_states.into_iter().chunks(chunk_size);
-    let worker_state_chunks = chunks
+    requests
         .into_iter()
-        .map(|c| c.into_iter().collect::<Vec<_>>());
-    for (reqs, states) in requests.chunks(chunk_size).zip(worker_state_chunks) {
-        let result = reqs
-            .into_iter()
-            .zip(states)
-            .map(|(req, state)| stage_fn(req, state))
-            .collect::<Vec<_>>();
-        thread_results.push(result);
-    }
-    thread_results.into_iter().flatten().collect::<Vec<_>>()
+        .zip(worker_states.into_iter())
+        .map(|(req, state)| stage_fn(req, state))
+        .collect::<Vec<_>>()
 }
--- mpi-snark/src/lib.rs
+++ mpi-snark/src/lib.rs
@@ -1,58 +1,8 @@
 use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
-use mpi::traits::Equivalence;
 
 pub mod coordinator;
 pub mod data_structures;
 pub mod worker;
-
-#[macro_export]
-macro_rules! construct_partitioned_buffer_for_scatter {
-    ($items:expr, $flattened_item_bytes: expr) => {{
-        let item_bytes = ($items)
-            .iter()
-            .map(serialize_to_packed_vec)
-            .collect::<Vec<_>>();
-        let counts = std::iter::once(&vec![])
-            .chain(item_bytes.iter())
-            .map(|bytes| bytes.len() as Count)
-            .collect::<Vec<_>>();
-        let displacements: Vec<Count> = counts
-            .iter()
-            .scan(0, |acc, &x| {
-                let tmp = *acc;
-                *acc += x;
-                Some(tmp)
-            })
-            .collect();
-        *$flattened_item_bytes = item_bytes.concat();
-        Partition::new(&*$flattened_item_bytes, counts, displacements)
-    }};
-}
-
-#[macro_export]
-macro_rules! construct_partitioned_mut_buffer_for_gather {
-    ($size: expr, $default: expr, $flattened_item_bytes: expr) => {{
-        let item_size = $default.uncompressed_size();
-        let item_bytes = std::iter::once(vec![])
-            .chain(std::iter::repeat(vec![0u8; item_size]))
-            .take($size as usize)
-            .collect::<Vec<_>>();
-        let counts = item_bytes
-            .iter()
-            .map(|bytes| bytes.len() as Count)
-            .collect::<Vec<_>>();
-        let displacements: Vec<Count> = counts
-            .iter()
-            .scan(0, |acc, &x| {
-                let tmp = *acc;
-                *acc += x;
-                Some(tmp)
-            })
-            .collect();
-        *$flattened_item_bytes = item_bytes.concat();
-        PartitionMut::new(&mut $flattened_item_bytes[..], counts, displacements)
-    }};
-}
 
 #[macro_export]
 macro_rules! deserialize_flattened_bytes {
@@ -69,44 +19,4 @@ pub fn serialize_to_vec(item: &impl CanonicalSerialize) -> Vec<u8> {
     let mut bytes = Vec::with_capacity(item.uncompressed_size());
     (*item).serialize_uncompressed(&mut bytes).unwrap();
     bytes
-}
-
-pub fn serialize_to_packed_vec(item: &impl CanonicalSerialize) -> Vec<Packed> {
-    serialize_to_vec(item)
-        .chunks(Packed::BYTE_SIZE)
-        .map(Packed::from_bytes)
-        .collect::<Vec<_>>()
-}
-
-#[derive(Equivalence, Copy, Clone)]
-#[repr(transparent)]
-pub struct Packed([u8; 256]);
-
-impl Packed {
-    pub const BYTE_SIZE: usize = ark_std::mem::size_of::<Packed>();
-
-    #[inline(always)]
-    pub const fn zero() -> Self {
-        Packed([0u8; 256])
-    }
-
-    #[inline(always)]
-    pub fn from_bytes(bytes: &[u8]) -> Self {
-        let mut packed = [0u8; 256];
-        packed[..bytes.len()].copy_from_slice(bytes);
-        Packed(packed)
-    }
-}
-
-pub fn packed_to_bytes(packed: &[Packed]) -> &[u8] {
-    let len = packed.len() * Packed::BYTE_SIZE;
-    let ptr = packed.as_ptr() as *const u8;
-    unsafe { std::slice::from_raw_parts(ptr, len) }
-}
-
-pub fn deserialize_from_packed_bytes<T: CanonicalDeserialize>(
-    packed: &[Packed],
-) -> Result<T, ark_serialize::SerializationError> {
-    let bytes = packed_to_bytes(packed);
-    T::deserialize_uncompressed_unchecked(bytes)
 }
